11cbcbc16e6655e88a487369847ab13997d218b8
Makis Otman <makisotman@gmail.com>
2015-04-19 00:14:37 +0100

    Extend make_move to cover edge case for when position is taken

--- lib/board.ex
+++ lib/board.ex
@@ -5,10 +5,26 @@ defmodule Board do
   def new, do: board(@three_by_three)
   def new(@four_by_four), do: board(@four_by_four)
 
-  def make_move(position, mark, board) when position >= 0, do: List.replace_at(board, position, mark)
-  def make_move(_, _, board), do: board
+  def make_move(position, mark, board) do
+    cond do
+      position_is_valid?(position, board) -> List.replace_at(board, position, mark)
+      true -> board
+    end
+  end
 
   defp board(size) do
     Enum.take(0..(size * size), (size * size))
+  end
+
+  defp position_is_valid?(position, board) do
+     position_not_taken(board, position) && position_is_within_bounds(position)
+  end
+
+  defp position_not_taken(board, position) do
+    Enum.at(board, position) |> is_number
+  end
+
+  defp position_is_within_bounds(position) do
+    position >= 0
   end
 end
--- test/board_test.exs
+++ test/board_test.exs
@@ -20,15 +20,22 @@ defmodule BoardTest do
                                                    6, 7, 8]
   end
 
-  test "returns board unchanged if move is invalid" do
+  test "returns board unchanged if position is invalid" do
     assert Board.make_move(10, "x", Board.new) == [0, 1, 2,
                                                    3, 4, 5,
                                                    6, 7, 8]
   end
 
-  test "returns board unchanged if move is negative" do
+  test "returns board unchanged if position is negative" do
     assert Board.make_move(-1, "x", Board.new) == [0, 1, 2,
                                                    3, 4, 5,
                                                    6, 7, 8]
+  end
+
+  test "returns board unchanged if position is taken" do
+    board_with_move = Board.make_move(0, "x", Board.new)
+    assert Board.make_move(0, "o", board_with_move) == ["x", 1, 2,
+                                                         3, 4, 5,
+                                                         6, 7, 8]
   end
 end
