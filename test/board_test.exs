e6de8e4e6c6f13cb4ed3788c6003d46e738ffccc
Makis Otman <makisotman@gmail.com>
2015-04-19 20:25:28 +0100

    Refactor Board tests

--- test/board_test.exs
+++ test/board_test.exs
@@ -1,53 +1,32 @@
 defmodule BoardTest do
   use ExUnit.Case
 
-  test "supports 3x3 by default" do
-    assert Board.new == ["", "", "",
-                         "", "", "",
-                         "", "", ""]
-  end
-
-  test "size can vary" do
-    assert Board.new(4) == ["", "", "", "",
-                            "", "", "", "",
-                            "", "", "", "",
-                            "", "", "", ""]
-  end
-
   test "marks a position on the board" do
-    assert Board.make_move(0, "x", Board.new) == ["x", "", "",
-                                                   "", "", "",
-                                                   "", "", ""]
+    assert make_move_on_empty_board(0, "x") == ["x", "", "", "", "", "", "", "", ""]
   end
 
-  test "returns board unchanged if position is invalid" do
-    assert Board.make_move(10, "x", Board.new) == ["", "", "",
-                                                   "", "", "",
-                                                   "", "", ""]
+  test "board is unchanged if position is invalid" do
+    assert make_move_on_empty_board(10, "x") == empty_board
   end
 
-  test "returns board unchanged if position is negative" do
-    assert Board.make_move(-1, "x", Board.new) == ["", "", "",
-                                                   "", "", "",
-                                                   "", "", ""]
+  test "board is unchanged if position is negative" do
+    assert make_move_on_empty_board(-1, "x") == empty_board
   end
 
-  test "returns board unchanged if position is taken" do
-    board_with_move = Board.make_move(0, "x", Board.new)
-    assert Board.make_move(0, "o", board_with_move) == ["x", "", "",
-                                                         "", "", "",
-                                                         "", "", ""]
+  test "board is unchanged if position is taken" do
+    board_with_one_move = make_move_on_empty_board(0, "x")
+    assert Board.make_move(0, "o", board_with_one_move) == board_with_one_move
   end
 
   test "returns the available moves" do
-    board_with_move = Board.make_move(5, "x", Board.new)
+    board_with_move = make_move_on_empty_board(5, "x")
     assert Board.available_moves(board_with_move) == [0, 1, 2, 3, 4, 6, 7, 8]
   end
 
   test "returns the rows for 3x3" do
-    assert Board.rows(Board.new) == [["", "", ""],
-                                     ["", "", ""],
-                                     ["", "", ""]]
+    assert Board.rows(empty_board) == [["", "", ""],
+                                       ["", "", ""],
+                                       ["", "", ""]]
   end
 
   test "returns the rows for 4x4" do
@@ -56,4 +35,6 @@ defmodule BoardTest do
                                         ["", "", "", ""],
                                         ["", "", "", ""]]
   end
+  defp empty_board(), do: Board.new
+  defp make_move_on_empty_board(move, mark), do: Board.make_move(move, mark, Board.new)
 end
