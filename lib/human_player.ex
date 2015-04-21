adae950474927ba7746e34cfc0ae2b686c7b968d
Makis Otman <makisotman@gmail.com>
2015-04-21 19:04:47 +0100

    Add HumanPlayer with function that returns a move

---
+++ lib/human_player.ex
@@ -0,0 +1,6 @@
+defmodule HumanPlayer do
+
+  def get_move(%{display: display}) do
+    display.get_move
+  end
+end
---
+++ test/human_player_test.exs
@@ -0,0 +1,12 @@
+defmodule HumanPlayerTest do
+  use ExUnit.Case
+  import ExUnit.CaptureIO
+
+  test "returns a move" do
+    game = %{board: [], display: Display}
+
+    assert capture_io([input: "1", capture_prompt: false], fn ->
+      IO.write HumanPlayer.get_move(game)
+    end) == "0"
+  end
+end
