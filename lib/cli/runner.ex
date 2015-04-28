9dbefa67da4b83dac025b290cb43c8016b6aa8d2
Makis Otman <makisotman@gmail.com>
2015-04-28 08:47:19 +0100

    Add exception handling in the Runner
    This catches the CTRL-D combination which detaches (exits) the Erlang
    shell. I couldn't find a way of doing the same for CTRL-C and I don't
    think you're supposed to anyway because of the fact that you can
    continue the process even after pressing it.

--- lib/cli/runner.ex
+++ lib/cli/runner.ex
@@ -2,19 +2,24 @@ defmodule CLI.Runner do
   @game_status_message 1
 
   def start(%{game: game, display: display}) do
-    game.play(%{board: get_board, display: display})
-    |> get_result_message
-    |> show_message(display)
-    display
-    |> user_wants_rematch
-    |> another_round(%{game: game, display: display})
+    try do
+      game.play(%{board: get_board, display: display})
+      |> get_result_message
+      |> show_message(display)
+      display
+      |> user_wants_rematch
+      |> another_round(%{game: game, display: display})
+    rescue
+      ErlangError -> show_farewell_message(display)
+    end
   end
 
   def another_round(:yes, setup),              do: start(setup)
-  def another_round(:no, %{display: display}), do: display.show_message("Thanks for playing!")
+  def another_round(:no, %{display: display}), do: show_farewell_message(display)
 
   defp get_board,                      do: Board.new
   defp get_result_message(game),       do: elem(game, @game_status_message)
   defp show_message(message, display), do: display.show_message(message)
   defp user_wants_rematch(display),    do: display.get_rematch_choice
+  defp show_farewell_message(display), do: display.show_message("Thanks for playing!")
 end
